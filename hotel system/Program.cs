1412f344449a8db98640a13ad0a5e5a007623427
mohammedyk7 <mohammedyk7@gmail.com>
2025-08-11 09:29:50 +0400

    find highest paying guest

--- hotel system/Program.cs
+++ hotel system/Program.cs
@@ -27,7 +27,7 @@ namespace SimpleHotelRoomManagement
                 Console.WriteLine("3) Reserve a room");//reserve a room
                 Console.WriteLine("4) View all reservations");//view all reservation 
                 Console.WriteLine("5) Search reservation by guest name");//search reservation by guest name
-                Console.WriteLine("6) Find highest-paying guest");
+                Console.WriteLine("6) Find highest-paying guest");//find highest paying guest
                 Console.WriteLine("7) Cancel a reservation by room number");
                 Console.WriteLine("8) Exit");
                 Console.Write("Choose (1-8): ");
