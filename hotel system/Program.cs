1baaf06a20bd23359bb903ad73844fa5833ff931
mohammedyk7 <mohammedyk7@gmail.com>
2025-08-11 09:30:58 +0400

    case 2: view all rooms

--- hotel system/Program.cs
+++ hotel system/Program.cs
@@ -37,7 +37,7 @@ namespace SimpleHotelRoomManagement
                 switch (choice)
                 {
                     case "1": AddRoom(); break;//case 1: add a new room
-                    case "2": ViewAllRooms(); break;
+                    case "2": ViewAllRooms(); break; //case 2: view all rooms
                     case "3": ReserveRoom(); break;
                     case "4": ViewAllReservations(); break;
                     case "5": SearchByGuestName(); break;
